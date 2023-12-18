feb0b000cb3163d2f1a39b5eeacc6e94381f9fb8
KirstyGong <kirstygong23@gmail.com>
2023-12-19 05:39:11 +1300

    Refactor InputReaderTest

--- src/test/java/prj/calculator/reader/InputReaderTest.java
+++ src/test/java/prj/calculator/reader/InputReaderTest.java
@@ -1,20 +1,17 @@
 package prj.calculator.reader;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
-import prj.calculator.util.IValidator;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class InputReaderTest {
     private InputReader inputReader;
@@ -31,15 +28,16 @@ public class InputReaderTest {
     })
     void testCanReadInput(String inputStr) throws IOException {
         //Given
-
-        try (InputStream input = new ByteArrayInputStream(inputStr.getBytes())) {
+        final String formatInputStr = String.format("%1$s\n%1$s\n%1$s", inputStr);
+        try (InputStream input = new ByteArrayInputStream(formatInputStr.getBytes())) {
             System.setIn(input);
 
             //When
-            final String result = inputReader.getInput();
+            final List<String> result = inputReader.getInput();
 
             //Then
-            assertEquals(inputStr, result);
+            final List<String> expected = List.of(inputStr, inputStr, inputStr);
+            assertEquals(expected, result);
         }
     }
 
